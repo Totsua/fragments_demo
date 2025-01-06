b448240a2bfc7d1d7fcc1e80102426968ad66b3a
Shamar <Shamar098@gmail.com>
2025-01-06 12:25:07 +0000

    feat: add Navigation bar implementation to Main

--- app/src/main/java/com/northcoders/fragments_demo/MainActivity.java
+++ app/src/main/java/com/northcoders/fragments_demo/MainActivity.java
@@ -1,24 +1,35 @@
 package com.northcoders.fragments_demo;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    NavigationBarView navigationBarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Set the Navigation Bar View to the one in main
+        navigationBarView = findViewById(R.id.BottomNavView);
+        // Set a listener that will be alerted when there is a selection on the navigation bar
+        navigationBarView.setOnItemSelectedListener(this);
+        // Set the first selected item as home
+        navigationBarView.setSelectedItemId(R.id.home);
+
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
