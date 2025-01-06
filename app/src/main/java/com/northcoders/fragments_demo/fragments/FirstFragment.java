3e406b42f234b4e5dbade0c130e7d3028b5329e6
Shamar <Shamar098@gmail.com>
2025-01-06 11:38:43 +0000

    refactor: move Fragments to fragments package

--- app/src/main/java/com/northcoders/fragments_demo/fragments/FirstFragment.java
+++ app/src/main/java/com/northcoders/fragments_demo/fragments/FirstFragment.java
@@ -1,4 +1,4 @@
-package com.northcoders.fragments_demo;
+package com.northcoders.fragments_demo.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.northcoders.fragments_demo.R;
 
 
 public class FirstFragment extends Fragment {
