e94ef57d2a8977e15c582365f47389ec2cd80dca
Shamar <Shamar098@gmail.com>
2025-01-06 12:25:38 +0000

    feat: add Navigation bar logic to Main

--- app/src/main/java/com/northcoders/fragments_demo/MainActivity.java
+++ app/src/main/java/com/northcoders/fragments_demo/MainActivity.java
@@ -7,10 +7,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.navigation.NavigationBarView;
+import com.northcoders.fragments_demo.fragments.FirstFragment;
+import com.northcoders.fragments_demo.fragments.SecondFragment;
+import com.northcoders.fragments_demo.fragments.ThirdFragment;
 
 public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
     NavigationBarView navigationBarView;
+    FirstFragment firstFragment = new FirstFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,32 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        // Set the page to change to home(firstFragment) if "home" is selected
+        if(item.getItemId() == R.id.home){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frameLayoutFragment, firstFragment)
+                    .commit();
+            return true;
+        }
+        // Set the page to change to add(secondFragment) if "add" is selected
+        if(item.getItemId() == R.id.add){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frameLayoutFragment, secondFragment)
+                    .commit();
+            return true;
+        }
+        // Set the page to change to favourites(thirdFragment) if "favourites" is selected
+        if(item.getItemId() == R.id.favourites){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frameLayoutFragment, thirdFragment)
+                    .commit();
+            return true;
+        }
+
         return false;
     }
 }
